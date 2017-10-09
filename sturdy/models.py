70c80e3746e82a2520945df0e49614461775b2fa
Andrei Tcibin <andrei_tcibin@epam.com>
2017-10-09 22:48:55 +0300

    feat: Add __str__ methods for Project and Task models

--- sturdy/models.py
+++ sturdy/models.py
@@ -3,14 +3,19 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     class Meta:
-        verbose_name = 'Команда'
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
 
     name = models.CharField(max_length = 200)
     created_date = models.DateField(auto_now = True)
 
+    def __str__(self):
+        return '%s' % (self.name,)
+
 class Task(models.Model):
     class Meta:
         verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
 
     title = models.CharField(max_length = 200)
     description = models.TextField()
@@ -27,3 +32,9 @@ class Task(models.Model):
         blank = True,
         null = True,
     )
+
+    def __str__(self):
+        if (self.parent == None):
+            return '%s' % (self.title,)
+        else:
+            return '%s >> %s' % (self.parent, self.title,)
