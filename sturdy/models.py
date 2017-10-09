0bf82f5069f261f9e8f7244e98e67c15f0f6097d
Andrei Tcibin <andrei_tcibin@epam.com>
2017-10-09 22:25:54 +0300

    feat: Add Project and Task models

--- sturdy/models.py
+++ sturdy/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    class Meta:
+        verbose_name = 'Команда'
+
+    name = models.CharField(max_length = 200)
+    created_date = models.DateField(auto_now = True)
+
+class Task(models.Model):
+    class Meta:
+        verbose_name = 'Задание'
+
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    created_date = models.DateField(auto_now = True)
+    story_points = models.PositiveIntegerField()
+    value_points = models.PositiveIntegerField()
+    project = models.ForeignKey(
+        Project,
+        on_delete = models.CASCADE
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete = models.SET_NULL,
+        blank = True,
+        null = True,
+    )
